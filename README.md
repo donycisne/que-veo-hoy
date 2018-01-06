fa0e389b363e336eeb5388266c34eb5598752fb3
Dony Cisneros <dony.cisne@gmail.com>
2018-01-05 23:30:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Que veo hoy - Project
---
+++ servidor/lib/conexionbd.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 
+  port     : 
+  user     : 
+  password : 
+  database : 
+});
+
+module.exports = connection;
+
